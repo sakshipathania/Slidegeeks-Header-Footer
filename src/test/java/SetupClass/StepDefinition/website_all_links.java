80cda2ec6a04e4f986f0b8d22049e140118dbac2
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-08-14 17:00:37 +0530

    Update website_all_links.java

--- src/test/java/SetupClass/StepDefinition/website_all_links.java
+++ src/test/java/SetupClass/StepDefinition/website_all_links.java
@@ -331,7 +331,7 @@ public class website_all_links extends Setup {
 		js.executeScript("arguments[0].scrollIntoView();",Resume_footer);
 		Resume_footer.click();
 		log.info("FOOTER --> RESUME SERVICES");
-		Thread.sleep(2000);
+		Thread.sleep(10000);
 		WebElement Resume_footer1= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Custom Resume Builder'][contains(.,'Contact&nbsp;Us')]")));
 		Resume_footer1.click();
 		Thread.sleep(3000);
