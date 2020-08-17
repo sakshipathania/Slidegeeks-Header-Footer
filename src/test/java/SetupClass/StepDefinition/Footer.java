bb3915d629ed95b71c073fb8c79bee6f4b6fc0bc
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-08-17 16:56:33 +0530

    Update Footer.java

--- src/test/java/SetupClass/StepDefinition/Footer.java
+++ src/test/java/SetupClass/StepDefinition/Footer.java
@@ -279,7 +279,7 @@ public class Footer extends Setup {
 	public void Certificate_PPT() throws Throwable {
 		
 		
-		  WebElement footer_12= wait.until(ExpectedConditions.elementToBeClickable(By.csselector("body > div.page-wrapper > footer > div > div > div.footer-outer > div > div:nth-child(3) > ul > li:nth-child(5) > a")));
+		  WebElement footer_12= wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("body > div.page-wrapper > footer > div > div > div.footer-outer > div > div:nth-child(3) > ul > li:nth-child(5) > a")));
 		  Thread.sleep(3000); 
 		js.executeScript("arguments[0].scrollIntoView();",footer_12);
 		  footer_12.click();
