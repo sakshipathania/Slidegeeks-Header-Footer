30c93acbbfa850d95514705460a52d6dde74f293
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-09-04 09:12:01 +0530

    Update website_all_links.java

--- src/test/java/SetupClass/StepDefinition/website_all_links.java
+++ src/test/java/SetupClass/StepDefinition/website_all_links.java
@@ -96,7 +96,7 @@ public class website_all_links extends Setup {
 	@Then("^user enter email and password epu$")
        public void user_enter_email_and_password_epu() throws InterruptedException  {
     
-	       WebElement old_paid_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='email']")));
+	        WebElement old_paid_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[2]/form/fieldset/div[2]/div/input")));
                old_paid_email.sendKeys("sakshi.pathania@slidetech.in");
     
                WebElement old_paid_pass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div[2]/div[2]/div[2]/form/fieldset/div[3]/div/input")));
