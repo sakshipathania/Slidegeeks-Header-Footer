993e41f2f239c73eab5abfe5175829ea5f93577b
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-15 15:55:51 +0530

    Update TakeImageStep.java

--- src/test/java/SetupClass/StepDefinition/TakeImageStep.java
+++ src/test/java/SetupClass/StepDefinition/TakeImageStep.java
@@ -1,21 +1,20 @@
-package SetupClass.StepDefinition;
+package TestRunner.loginSteps;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import SetupClass.Setup;
+import TestRunner.SetupClass;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 
-public class TakeImageStep extends Setup{
 
-	@After
-	public void tearDown(Scenario scenario) {
-	    if (scenario.isFailed()) {	    	
-	      log.info("Scenario failed, now taking screenshot");
-	      final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-	      scenario.embed(screenshot, "image/png");
-	    }
+	public class TakeImageStep extends SetupClass {
+		@After
+		public void tearDown(Scenario scenario) {
+		    if (scenario.isFailed()) {	    	
+		      log.info("Scenario failed, now taking screenshot");
+		      final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+		      scenario.embed(screenshot, "image/png");
+		    }
+		}
 	}
-	
-}
